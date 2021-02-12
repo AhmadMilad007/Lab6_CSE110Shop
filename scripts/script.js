821678593955340f1df66fcf4055ddf3fb910d51
AhmadMilad007 <60712763+AhmadMilad007@users.noreply.github.com>
2021-02-12 11:51:06 -0800

    Update script.js
    Updated scripts js

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,37 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+	let intial
+	if (localStorage.getItem('dataArray')==null ) {
+
+		fetch ('https://fakestoreapi.com/products')
+		.then (response => response.json())	
+		.then (data => localStorage.setItem('dataArray', JSON.stringify(data))); 
+		
+		loader();
+
+	} 
+	else 
+	{
+
+		loader();	
+	}
+
+
 });
+
+function loader () {
+		
+		const item_list 	= document.querySelector ('#product-list');
+		let product 		= JSON.parse(localStorage.getItem('dataArray'));
+		for (let i = 0 ; i < product.length; i ++ ) {
+			let childProduct = document.createElement ('child');
+			childProduct.title = product[i].title;
+			childProduct.img   = product[i].img;
+			childProduct.price = product[i].price;
+			childProduct.id    = product[i].id;
+			item_list.appendChild (childProduct);
+			console.log(childProduct.price);  //for testing- take it out before submission 
+		} 	 
+	
+	}
