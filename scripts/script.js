8c8855641000e66b096372d592ebdfac08ba9624
AhmadMilad007 <60712763+AhmadMilad007@users.noreply.github.com>
2021-02-13 00:07:08 -0800

    Update script.js
    Fixed the bug, need to work on product items

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,20 @@
 // Script.js
-
+async function dataFetch(){
+  let items = JSON.parse(localStorage.getItem('productList'));
+  if (items.length == 0) {
+    const response = await fetch('https://fakestoreapi.com/products');
+    items =  await response.json();
+  }
+  return items;
+}
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
-	let intial
-	if (localStorage.getItem('dataArray')==null ) {
-
-		fetch ('https://fakestoreapi.com/products')
-		.then (response => response.json())	
-		.then (data => localStorage.setItem('dataArray', JSON.stringify(data))); 
-		
-		loader();
-
-	} 
-	else 
-	{
-
-		loader();	
-	}
-
-
+ let itemList = document.querySelector('#product-list');
+    dataFetch().then((products) => {
+         localStorage.setItem('productList', JSON.stringify(products));
+        for (let i = 0 ; i < products.length; i++){
+          if (products[i]!=null){
+          itemList.appendChild(new ProductItem(products[i].id, products[i].image, products[i].title, products[i].price));
+        }
+      }
+  });
 });
-
-function loader () {
-		
-		const item_list 	= document.querySelector ('#product-list');
-		let product 		= JSON.parse(localStorage.getItem('dataArray'));
-		for (let i = 0 ; i < product.length; i ++ ) {
-			let childProduct = document.createElement ('child');
-			childProduct.title = product[i].title;
-			childProduct.img   = product[i].img;
-			childProduct.price = product[i].price;
-			childProduct.id    = product[i].id;
-			item_list.appendChild (childProduct);
-			console.log(childProduct.price);  //for testing- take it out before submission 
-		} 	 
-	
-	}
